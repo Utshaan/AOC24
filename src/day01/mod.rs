b1b44e3c2866f02e7078607d6dd91cbc8a4e8881
Utkarsh Thanvi <omninight.business@gmail.com>
2024-12-02 07:20:22 +0530

    Finished Day 01

---
+++ src/day01/mod.rs
@@ -0,0 +1,39 @@
+use std::collections::{BinaryHeap, HashMap};
+use std::fs;
+
+use crate::print_day;
+
+pub fn solve() {
+    print_day("DAY 01");
+    let content = fs::read_to_string("src/day01/input.txt").expect(
+        "Issue in reading input.txt. Make sure the file exists and the permissions are right",
+    );
+
+    let mut left: BinaryHeap<u32> = BinaryHeap::new();
+    let mut right: BinaryHeap<u32> = BinaryHeap::new();
+    let mut map: HashMap<u32, u32> = HashMap::new();
+
+    content.lines().into_iter().for_each(|x| {
+        let pair: Vec<u32> = x
+            .split_whitespace()
+            .map(|num| num.parse::<u32>().unwrap())
+            .collect();
+        left.push(pair[0]);
+        right.push(pair[1]);
+
+        map.entry(pair[1])
+            .and_modify(|count| *count += 1)
+            .or_insert(1);
+    });
+
+    let mut diff_score = 0;
+    let mut sim_score = 0;
+
+    while let (Some(left_val), Some(right_val)) = (left.pop(), right.pop()) {
+        diff_score += left_val.abs_diff(right_val);
+        sim_score += left_val * map.get(&left_val).unwrap_or(&0);
+    }
+
+    println!("diff score = {}", diff_score);
+    println!("sim score = {}", sim_score);
+}
---
+++ src/main.rs
@@ -0,0 +1,21 @@
+pub mod day01;
+
+static mut WIDTH: u16 = 0;
+
+fn main() {
+    unsafe {
+        WIDTH = termion::terminal_size().unwrap().0;
+    }
+    day01::solve();
+}
+
+fn print_day(day: &str) {
+    unsafe {
+        println!(
+            " {} {} {} \n",
+            (5..WIDTH / 2).map(|_| '-').collect::<String>(),
+            day,
+            (5..WIDTH / 2).map(|_| '-').collect::<String>()
+        );
+    }
+}
